39ead253e6930a9efaaff2d33d7468d043128bba
Denis <dddenislav@gmail.com>
2025-05-27 23:06:50 +0300

    Fix build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,32 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vuetify({ autoImport: true }),
   ],
+  define: { 'process.env': {} },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': '/src',
+    },
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      external: ['vue', 'vue-router'],
+    },
+  },
+  optimizeDeps: {
+    include: ['vue', 'vue-router', 'vuetify'],
+  },
+});
